d0382c6b26fbc2565608fb72eb6eee1a6519b1c2
Yeo Yu Xuan <yeoyuxuan302@gmail.com>
2025-08-01 23:03:12 +0800

    Store dialect dictionary constants (kelantan & sarawak malay)

---
+++ ULTRA/src/dictionary/sarawak.ts
@@ -0,0 +1,11 @@
+{/* Version 1.0 still to be update */}
+export const SARAWAK_DICTIONARY = {
+    'aek': ['air'],
+    'agak situk lok': ['mari sini sekejap'],
+    'aku agik gago': ['saya tengah sibuk'],
+    'ambal': ['siput buluh'],
+    'ambik': ['mengambil'],
+    'angoi': ['pening', 'kepala serabut', 'bosan', 'fikiran kusut'],
+    'angol': ['pening kepala'],
+    'anok': ['marah, ejek'],
+} as const
