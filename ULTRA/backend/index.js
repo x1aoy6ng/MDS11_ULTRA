2cf18762b0b6439f9943746a5f9e70bfcf712f5c
jx <jxjx0110@gmail.com>
2025-08-24 18:07:24 +0800

    Enhance file upload and history, add music-metadata dependency, and update gitignore

--- ULTRA/backend/index.js
+++ ULTRA/backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
+const musicmd = require("music-metadata"); // to get audio metadata
 
 const app = express();
 const upload = multer({ dest: "uploads/" }); // files go into uploads/
@@ -20,12 +21,22 @@ app.listen(port, () => {
 });
 
 // Upload route, .single because single file upload
-app.post("/upload", upload.single("file"), (req, res) => {
+app.post("/upload", upload.single("file"), async(req, res) => {
   const file = req.file; //info about the uploaded file
+  const metadata = await musicmd.parseFile(file.path);
+  const duration = metadata.format.duration; // in seconds
+
+  if (!file) {
+    return res.status(400).json({ success: false, message: "No file uploaded" });
+  }
+
   const record = {
     fileName: file.originalname,
     path: file.path,
+    size: `${(file.size / (1024 * 1024)).toFixed(2)} MB`,
+    status: "Processing",
     uploadDate: new Date().toISOString(),
+    duration: formatDuration(duration),
   };
   history.push(record);
   res.json({ success: true, file: record });
@@ -35,4 +46,19 @@ app.post("/upload", upload.single("file"), (req, res) => {
 app.get("/history", (req, res) => {
   res.json(history);
 });
+
+// Helper function to format duration in seconds to HH:MM:SS
+function formatDuration(seconds) {
+  if (!seconds || isNaN(seconds)) return "--:--:--";
+
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = Math.floor(seconds % 60);
+
+
+  return `${hrs.toString().padStart(2, "0")}:${mins
+    .toString()
+    .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+}
+
 
