d0f107d8fa8a328661fd205c2012ce16e6c5ad86
jx <jxjx0110@gmail.com>
2025-08-24 18:40:33 +0800

    Refactor backend structure: move routes and upload logic to separate routes.js file

--- ULTRA/backend/index.js
+++ ULTRA/backend/index.js
@@ -1,64 +1,14 @@
 const express = require("express");
-const multer = require("multer");
 const cors = require("cors");
-const musicmd = require("music-metadata"); // to get audio metadata
 
 const app = express();
-const upload = multer({ dest: "uploads/" }); // files go into uploads/
-
 app.use(cors()); // allow React frontend to talk to backend
 
-let history = []; // store uploaded file info (temporary, in memory)
-
-// Test route
-app.get("/", (req, res) => {
-  res.send("Server is ready!");
-});
+//Import routes
+const routes = require("./routes");
+app.use("/", routes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
-
-// Upload route, .single because single file upload
-app.post("/upload", upload.single("file"), async(req, res) => {
-  const file = req.file; //info about the uploaded file
-  const metadata = await musicmd.parseFile(file.path);
-  const duration = metadata.format.duration; // in seconds
-
-  if (!file) {
-    return res.status(400).json({ success: false, message: "No file uploaded" });
-  }
-
-  const record = {
-    fileName: file.originalname,
-    path: file.path,
-    size: `${(file.size / (1024 * 1024)).toFixed(2)} MB`,
-    status: "Processing",
-    uploadDate: new Date().toISOString(),
-    duration: formatDuration(duration),
-  };
-  history.push(record);
-  res.json({ success: true, file: record });
-});
-
-// History route
-app.get("/history", (req, res) => {
-  res.json(history);
-});
-
-// Helper function to format duration in seconds to HH:MM:SS
-function formatDuration(seconds) {
-  if (!seconds || isNaN(seconds)) return "--:--:--";
-
-  const hrs = Math.floor(seconds / 3600);
-  const mins = Math.floor((seconds % 3600) / 60);
-  const secs = Math.floor(seconds % 60);
-
-
-  return `${hrs.toString().padStart(2, "0")}:${mins
-    .toString()
-    .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
-}
-
-
